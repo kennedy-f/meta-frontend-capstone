fbe0c945ccc987f3cb82d0256313d4886630c2bb
Kennedy Silva <kennedy2000silva@hotmail.com>
2023-02-05 20:21:36 -0300

    header with material

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,51 +1,38 @@
+import { Button, Container, Grid } from "@mui/material";
 import React from "react";
 
-import { Container, Grid, List, ListItem, ListItemButton } from "@mui/material";
+const navOptions: { label: string; href: string }[] = [
+  { label: "home", href: "/" },
+  { label: "about", href: "/" },
+  { label: "menu", href: "" },
+  { label: "reservations", href: "/" },
+  { label: "Order Online", href: "/" },
+  { label: "login", href: "/" },
+];
 
 export function Header() {
   return (
     <header style={{ width: "100%" }}>
       <Container maxWidth={"lg"}>
-        <Grid container xs={12} alignContent={"center"} sx={{ height: 100 }}>
+        <Grid
+          container
+          xs={12}
+          alignItems={"center"}
+          sx={{ height: "fit-content", pt: 2 }}
+        >
           <Grid item md={3}>
-            <img alt={"little lemon logo "} src={"/imgs/Logo .svg"} />
+            <img
+              alt={"little lemon logo "}
+              src={"/imgs/Logo .svg"}
+              height={60}
+            />
           </Grid>
-          <Grid item md={8}>
-            <List
-              component={"ul"}
-              sx={{ display: "flex", flexDirection: "row" }}
-            >
-              <ListItem>
-                <ListItemButton component={"a"} href={"/home"}>
-                  Home
-                </ListItemButton>
-              </ListItem>
-              <ListItem>
-                <ListItemButton component={"a"} href={"#ABOUT"}>
-                  About
-                </ListItemButton>
-              </ListItem>
-              <ListItem>
-                <ListItemButton component={"a"} href={"#MENU"}>
-                  Menu
-                </ListItemButton>
-              </ListItem>
-              <ListItem>
-                <ListItemButton component={"a"} href={"/booking"}>
-                  Reservations
-                </ListItemButton>
-              </ListItem>
-              <ListItem>
-                <ListItemButton component={"a"} href={"#ORDER-ONLINE"}>
-                  Order Online
-                </ListItemButton>
-              </ListItem>
-              <ListItem>
-                <ListItemButton component={"a"} href={"/login"}>
-                  Login
-                </ListItemButton>
-              </ListItem>
-            </List>
+          <Grid item md={8} container justifyContent={"flex-end"}>
+            {navOptions.map(({ label, href }) => (
+              <Button component={"a"} href={href}>
+                {label.toUpperCase()}
+              </Button>
+            ))}
           </Grid>
         </Grid>
       </Container>
