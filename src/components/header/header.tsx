791d598097336c4cebca846c772f96ca732a3769
Kennedy Silva <kennedy2000silva@hotmail.com>
2023-02-05 20:12:10 -0300

    fix: header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,36 +1,51 @@
 import React from "react";
 
-import { Container, Grid } from "@mui/material";
+import { Container, Grid, List, ListItem, ListItemButton } from "@mui/material";
 
 export function Header() {
   return (
-    <header>
+    <header style={{ width: "100%" }}>
       <Container maxWidth={"lg"}>
-        <Grid container xs={12}>
+        <Grid container xs={12} alignContent={"center"} sx={{ height: 100 }}>
           <Grid item md={3}>
             <img alt={"little lemon logo "} src={"/imgs/Logo .svg"} />
           </Grid>
           <Grid item md={8}>
-            <ul>
-              <li>
-                <a href={"/home"}> Home </a>
-              </li>
-              <li>
-                <a href={"#ABOUT"}> About </a>
-              </li>
-              <li>
-                <a href={"#MENU"}> Menu </a>
-              </li>
-              <li>
-                <a href={"/booking"}> Reservations </a>
-              </li>
-              <li>
-                <a href={"#ORDER-ONLINE"}> Order Online </a>
-              </li>
-              <li>
-                <a href={"/login"}> Login </a>
-              </li>
-            </ul>
+            <List
+              component={"ul"}
+              sx={{ display: "flex", flexDirection: "row" }}
+            >
+              <ListItem>
+                <ListItemButton component={"a"} href={"/home"}>
+                  Home
+                </ListItemButton>
+              </ListItem>
+              <ListItem>
+                <ListItemButton component={"a"} href={"#ABOUT"}>
+                  About
+                </ListItemButton>
+              </ListItem>
+              <ListItem>
+                <ListItemButton component={"a"} href={"#MENU"}>
+                  Menu
+                </ListItemButton>
+              </ListItem>
+              <ListItem>
+                <ListItemButton component={"a"} href={"/booking"}>
+                  Reservations
+                </ListItemButton>
+              </ListItem>
+              <ListItem>
+                <ListItemButton component={"a"} href={"#ORDER-ONLINE"}>
+                  Order Online
+                </ListItemButton>
+              </ListItem>
+              <ListItem>
+                <ListItemButton component={"a"} href={"/login"}>
+                  Login
+                </ListItemButton>
+              </ListItem>
+            </List>
           </Grid>
         </Grid>
       </Container>
